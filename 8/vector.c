c297d1b2075a047ac323751c434c609eb9f7998c
Alexander Smirnov <alecsmirnov93@gmail.com>
2019-06-08 18:55:35 +0700

    Growth factor fix

--- 8/vector.c
+++ 8/vector.c
@@ -1,26 +1,28 @@
 #include "vector.h"
 
 #include <stdlib.h>
+#include <math.h>
 
-#define GROWN_COEFFICIENT 1.5       // Коэффициент роса вектора (соответствует STL)
+#define GROWTH_FACTOR 1.5       // Коэффициент роса вектора (соответствует STL)
 
 // Инициализация вектора и его начальной вместимости
 void vectorInit(Vector* V, uint8_t capacity) {
     V->size = 0;
     V->capacity = capacity;
 
-    V->capacity ? (V->data = (Elem*)malloc(sizeof(Elem) * V->capacity)) :
+    V->capacity ? (V->data = (Elem*)malloc(V->capacity * sizeof(Elem))) :
                   (V->data = NULL);
 }
 
 // Добавить элемент в конец вектора
 void vectorAdd(Vector* V, Elem E) {
     if (V->size == V->capacity) {
-        V->capacity *= GROWN_COEFFICIENT;
+        V->capacity = (uint8_t)round(V->capacity * GROWTH_FACTOR);
+		
         if (V->size == V->capacity)
             ++V->capacity;
 
-        V->data = (Elem*)realloc(V->data, sizeof(Elem) * V->capacity);
+        V->data = (Elem*)realloc(V->data, V->capacity * sizeof(Elem));
     }
 
 	vectorElemAssign(&V->data[V->size++], E);
